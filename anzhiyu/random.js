cde909bf6ceffa105c74a8132df622b25100ed31
飞向月球的猫 <2194785560@qq.com>
2025-06-09 00:06:26 +0800

    Site updated: 2025-06-09 00:06:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/07/罗马数字转整数/"];function toRandomPost(){
+var posts=["2025/06/07/罗马数字转整数/","2025/06/08/引导程序/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
